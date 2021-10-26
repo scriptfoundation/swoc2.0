bc60de0a236591ec0617dff7e5ffb3f994b40bcd
pallavi1812 <upallavi1812@gmail.com>
2021-10-26 20:13:51 +0530

    added intern data

---
+++ src/components/js/intern-team.js
@@ -0,0 +1,91 @@
+import JaideepImg from '../img/Interns/Jaideep_S.jpg';
+import DivyaImg from '../img/Interns/Divya_S.png';
+import AnushkaImg from '../img/Interns/Anushka_G.jpeg';
+import JayashreeImg from '../img/Interns/Jayashree.jpeg';
+import MinalImg from '../img/Interns/Minal_M.jpeg';
+import SnehaImg from '../img/Interns/Sneha_M.jpg';
+
+export const internTeam = [
+    {
+        name: 'Jaideep Singh',
+        position: '',
+        image: JaideepImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/jaideep-singh-26a226208/',
+                icon: 'fa fa-linkedin'
+            }
+        ]
+    },
+
+    {
+        name: 'Minal Madhur ',
+        position: '',
+        image: MinalImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/minal-madhur-0434b4204',
+                icon: 'fa fa-linkedin',
+            }
+        ]
+    },
+    {
+        name: 'Udit Jain',
+        position: '',
+        image: JaideepImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/udit-kumar-jain-82121b1b0',
+                icon: 'fa fa-linkedin',
+            }
+        ]
+    },
+
+    {
+        name: 'Sneha Mathadawar',
+        position: '',
+        image: SnehaImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/sneha-mathadawar-0205851a2/',
+                icon: 'fa fa-linkedin',
+            },
+        ]
+    },
+
+    {
+        name: 'Jayashree Sadarhalli',
+        position: '',
+        image: JayashreeImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/jayashree-sadarhalli-42265b1b4',
+                icon: 'fa fa-linkedin',
+            }
+        ]
+    },
+
+    {
+        name: 'Anushka Gamad',
+        position: '',
+        image: AnushkaImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/anushka-gamad-2aa672208/',
+                icon: 'fa fa-linkedin',
+            },
+        ]
+    },
+
+    {
+        name: 'Divya Suri',
+        position: '',
+        image: DivyaImg,
+        profiles: [
+            {
+                link: 'https://www.linkedin.com/in/divya-suri-9b440b1aa/',
+                icon: 'fa fa-linkedin',
+            }
+        ]
+    }
+];
